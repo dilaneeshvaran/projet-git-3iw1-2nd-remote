c4f4c550e00b056f8483839c2d6d0762310df200
Dilan EESHVARAN <ieeshvarand@gmail.com>
2025-02-01 12:55:14 +0100

    GH-39 add products data
    https://github.com/dilaneeshvaran/projet-git-3iw1/issues/39
    
    Signed-off-by: Dilan EESHVARAN <ieeshvarand@gmail.com>

---
+++ git-app/src/Data/Products.tsx
@@ -0,0 +1,32 @@
+export const products = [
+    {
+        id: 1,
+        name: "Product 1",
+        price: 29.99,
+        description: "Description for Product 1"
+    },
+    {
+        id: 2,
+        name: "Product 2",
+        price: 49.99,
+        description: "Description for Product 2"
+    },
+    {
+        id: 3,
+        name: "Product 3",
+        price: 19.99,
+        description: "Description for Product 3"
+    },
+    {
+        id: 4,
+        name: "Product 4",
+        price: 99.99,
+        description: "Description for Product 4"
+    },
+    {
+        id: 5,
+        name: "Product 5",
+        price: 39.99,
+        description: "Description for Product 5"
+    }
+];

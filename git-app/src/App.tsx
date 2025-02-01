3122c4776e196821aaf3b9fca26e9b7a69ac9c2b
Dilan EESHVARAN <ieeshvarand@gmail.com>
2025-02-01 12:39:03 +0100

    GH-32 imrove home page
    Issue: https://github.com/dilaneeshvaran/projet-git-3iw1/issues/32
    
    Signed-off-by: Dilan EESHVARAN <ieeshvarand@gmail.com>

--- git-app/src/App.tsx
+++ git-app/src/App.tsx
@@ -1,13 +1,18 @@
-import './App.css'
+import './App.css';
+import Navbar from './Components/Navbar';
+import Products from './Components/Products';
+import Footer from './Components/Footer';
 
 function App() {
-
   return (
     <>
-      <div>
+      <Navbar />
+      <div className="content">
+        <Products />
       </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

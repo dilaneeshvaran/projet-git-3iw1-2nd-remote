43cdadf4e253e9d9aef90f056673b9e99ce2480b
Dilan EESHVARAN <ieeshvarand@gmail.com>
2025-02-01 12:45:54 +0100

    GH-35 add about us page
    Issue : https://github.com/dilaneeshvaran/projet-git-3iw1/issues/35
    
    Signed-off-by: Dilan EESHVARAN <ieeshvarand@gmail.com>

---
+++ git-app/src/Components/AboutUs.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AboutUs: React.FC = () => {
+    return (
+        <div className="about-us">
+            <h1>About Us</h1>
+            <p>Welcome to our company. We are dedicated to providing the best service possible.</p>
+            <h2>Our Mission</h2>
+            <p>Our mission is to deliver high-quality products that bring value to our customers.</p>
+            <h2>Our Team</h2>
+            <p>We have a diverse team of professionals who are passionate about what they do.</p>
+        </div>
+    );
+};
+
+export default AboutUs;
